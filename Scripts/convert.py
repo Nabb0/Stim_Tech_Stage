99b7eeacde63a990f753f16afb36087dc202c00d
DajaKlaus <91118237+DajaKlaus@users.noreply.github.com>
2023-06-19 16:01:17 +0200

    progetto con script di lettura completi

---
+++ Scripts/convert.py
@@ -0,0 +1,49 @@
+import fitz 
+import io
+import os
+
+#?Covenrsione PDF in txt
+#!Conversione file
+def convert_pdfs_in_folder(file, i):
+    # Apri il file PDF in modalità binaria
+    with open(file, 'rb') as pdf_file:
+        
+        # Leggi il contenuto del file PDF in una variabile di tipo bytes
+        pdf_bytes = pdf_file.read()
+
+    # Crea un oggetto lettore per il file PDF
+    pdf_doc = fitz.open(stream=io.BytesIO(pdf_bytes), filetype="pdf")
+
+    # Inizializza una stringa vuota per contenere il testo estratto
+    text = ''
+
+    # Loop attraverso tutte le pagine del file PDF
+    for page_num in range(pdf_doc.page_count):
+        
+        # Estrai il testo dalla pagina corrente
+        page = pdf_doc[page_num]
+        page_text = page.get_text()
+        
+        # Aggiungi il testo estratto alla stringa principale
+        text += page_text
+
+    # Scrivi la stringa contenente il testo estratto in un file di testo
+    with open('Txt/File/file{}.txt'.format(i), 'w') as txt_file:
+        txt_file.write(text)
+
+def convert():
+    directory = "E:/informatica/Visual Studio Code Projects/py projects/Stim Script/Pdf/File/"
+
+    file_list = os.listdir(directory)
+    i = 1
+    for file in file_list:
+        file_path = os.path.join(directory, file)
+        convert_pdfs_in_folder(file_path, i)
+        i += 1
+
+
+def main():
+    convert()
+
+
+main()
